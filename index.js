5c99c8695de032c9791f6aeec594188fd9d387df
Khushi Johri <65448358+khushijohri2001@users.noreply.github.com>
2021-08-16 12:17:59 +0000

    fix: spacing

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-var readlineSync= require('readline-sync');
+var readlineSync = require('readline-sync');
 
-var score=0;
+var score = 0;
 
 //user input
 var userName = readlineSync.question("What's your name? ");
 console.log("\n");
-console.log("Welcome "+ userName+ " to the game 'DO YOU KNOW KHUSHI?' ");
+console.log("Welcome " + userName + " to the game 'DO YOU KNOW KHUSHI?' ");
 console.log("\n");
 console.log("Answer 5 Questions about Khushi ");
 console.log("................................");
@@ -15,13 +15,13 @@ console.log("................................");
 
 
 //function definition
-function quiz(question, answer){
-var input= readlineSync.question(question);
-  if(input.toUpperCase() === answer.toUpperCase()){
+function quiz(question, answer) {
+  var input = readlineSync.question(question);
+  if (input.toUpperCase() === answer.toUpperCase()) {
     console.log("Right!");
-    score= score+1;
+    score = score + 1;
   }
-  else{
+  else {
     console.log("Wrong!");
   }
 
@@ -31,66 +31,65 @@ var input= readlineSync.question(question);
 
 
 //objects
-var questionOne={
-  question:"Where do Khushi Live? ",
-  answer:"jaipur"
+var questionOne = {
+  question: "Where do Khushi Live? ",
+  answer: "jaipur"
 }
 
-var questionTwo={
-  question:"What is Khushi's favourite colour? ",
-  answer:"blue"
+var questionTwo = {
+  question: "What is Khushi's favourite colour? ",
+  answer: "blue"
 }
 
-var questionThree={
-  question:"What is Khushi's age? ",
+var questionThree = {
+  question: "What is Khushi's age? ",
   answer: "19"
 }
-var questionFour={
-  question:"What is Khushi's Favourite hobby? ",
-  answer:"drawing"
+var questionFour = {
+  question: "What is Khushi's Favourite hobby? ",
+  answer: "drawing"
 }
-var questionFive={
-  question:"In which year was she born? ",
-  answer:"2001"
+var questionFive = {
+  question: "In which year was she born? ",
+  answer: "2001"
 }
 
 
 //array definition
-var ques= [questionOne, questionTwo, questionThree, questionFour, questionFive];
+var ques = [questionOne, questionTwo, questionThree, questionFour, questionFive];
 
 //function call
-for(var i=0; i<ques.length; i++)
-{
-  var currentQues=ques[i];
-quiz(currentQues.question, currentQues.answer);
+for (var i = 0; i < ques.length; i++) {
+  var currentQues = ques[i];
+  quiz(currentQues.question, currentQues.answer);
 }
 
 console.log("\n");
-console.log(userName +"Your score is "+ score+"/5");
+console.log(userName + "Your score is " + score + "/5");
 console.log("\n");
 
 
-if(score == 5){
-  console.log("Wowwww! "+userName+" , you are closest to Khushi");
+if (score == 5) {
+  console.log("Wowwww! " + userName + " , you are closest to Khushi");
 }
-else if(score == 4){
-  console.log("Amazing! "+userName+" , you are close enough to Khushi");
+else if (score == 4) {
+  console.log("Amazing! " + userName + " , you are close enough to Khushi");
 }
- else if(score == 3){
-  console.log("Good! "+userName+" , you know Khushi");
+else if (score == 3) {
+  console.log("Good! " + userName + " , you know Khushi");
 }
-else if(score == 2){
-  console.log("Fine! "+userName+" You dont know Khushi well");
+else if (score == 2) {
+  console.log("Fine! " + userName + " You dont know Khushi well");
 }
-else{
-  console.log("Oh Noooo! "+userName+" you dont know Khushi at all");
+else {
+  console.log("Oh Noooo! " + userName + " you dont know Khushi at all");
 }
 
 
 
 console.log("Thank you for playing");
 
-var highscore= [
+var highscore = [
   {
     name: "Khushi",
     score: 5
@@ -102,10 +101,10 @@ var highscore= [
   }
 ]
 
-for( var k=0; k<highscore.length; k++){
-  currentScore=highscore[k];
-  t=k+1;
-console.log("The scorer "+ t +" is "+ currentScore.name +" with a score of " +currentScore.score);
+for (var k = 0; k < highscore.length; k++) {
+  currentScore = highscore[k];
+  t = k + 1;
+  console.log("The scorer " + t + " is " + currentScore.name + " with a score of " + currentScore.score);
 
 }
 
